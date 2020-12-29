42a6867b01dba678b0c3364ef01e3c057a2b2b83
suhrobcoder <suhrobcoder@gmail.com>
2020-12-29 06:32:02 +0500

    DI setup

--- app/src/main/java/uz/suhrob/musicplayerapp/MusicPlayerApplication.kt
+++ app/src/main/java/uz/suhrob/musicplayerapp/MusicPlayerApplication.kt
@@ -1,8 +1,10 @@
 package uz.suhrob.musicplayerapp
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 
+@HiltAndroidApp
 class MusicPlayerApplication :Application() {
     override fun onCreate() {
         super.onCreate()
---
+++ app/src/main/java/uz/suhrob/musicplayerapp/di/AppModule.kt
@@ -0,0 +1,27 @@
+package uz.suhrob.musicplayerapp.di
+
+import android.content.Context
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+import dagger.hilt.android.qualifiers.ApplicationContext
+import uz.suhrob.musicplayerapp.R
+import javax.inject.Singleton
+
+@InstallIn(ApplicationComponent::class)
+@Module
+object AppModule {
+    @Singleton
+    @Provides
+    fun provideGlideInstance(
+        @ApplicationContext context: Context
+    ) = Glide.with(context).setDefaultRequestOptions(
+        RequestOptions()
+            .placeholder(R.drawable.ic_song)
+            .error(R.drawable.ic_song)
+            .centerCrop()
+    )
+}
