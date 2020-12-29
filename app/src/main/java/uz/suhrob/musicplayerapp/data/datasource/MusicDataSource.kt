d483a8a28d7c5147591441ee1d968dc24389ea62
suhrobcoder <suhrobcoder@gmail.com>
2020-12-29 08:52:08 +0500

    MusicDataSource setup

--- app/src/main/java/uz/suhrob/musicplayerapp/data/datasource/MusicDataSource.kt
+++ app/src/main/java/uz/suhrob/musicplayerapp/data/datasource/MusicDataSource.kt
@@ -4,16 +4,94 @@ import android.content.Context
 import android.graphics.BitmapFactory
 import android.media.MediaMetadataRetriever
 import android.provider.MediaStore
+import android.support.v4.media.MediaBrowserCompat
+import android.support.v4.media.MediaBrowserCompat.MediaItem.FLAG_PLAYABLE
+import android.support.v4.media.MediaDescriptionCompat
+import android.support.v4.media.MediaMetadataCompat
+import android.support.v4.media.MediaMetadataCompat.*
+import androidx.core.net.toUri
 import androidx.loader.content.CursorLoader
+import com.google.android.exoplayer2.source.ConcatenatingMediaSource
+import com.google.android.exoplayer2.source.ProgressiveMediaSource
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
 import uz.suhrob.musicplayerapp.data.model.Song
 
 class MusicDataSource(
     private val context: Context
 ) {
+
+    private var songs = emptyList<MediaMetadataCompat>()
+
+    fun fetchMediaMetadata() {
+        state = State.STATE_INITIALIZING
+        val allSongs = getAllSongs()
+        songs = allSongs.map { song ->
+            Builder()
+                .putString(METADATA_KEY_ARTIST, song.artist)
+                .putString(METADATA_KEY_MEDIA_ID, song.mediaId)
+                .putString(METADATA_KEY_TITLE, song.title)
+                .putString(METADATA_KEY_DISPLAY_TITLE, song.title)
+                .putString(METADATA_KEY_MEDIA_URI, song.path)
+                .putString(METADATA_KEY_DISPLAY_SUBTITLE, song.artist)
+                .putString(METADATA_KEY_DISPLAY_DESCRIPTION, song.artist)
+                .putBitmap(METADATA_KEY_ALBUM_ART, song.image)
+                .putBitmap(METADATA_KEY_DISPLAY_ICON, song.image)
+                .build()
+        }
+        state = State.STATE_INITIALIZED
+    }
+
+    fun asMediaSource(dataSourceFactory: DefaultDataSourceFactory): ConcatenatingMediaSource {
+        val concatenatingMediaSource = ConcatenatingMediaSource()
+        songs.forEach { song ->
+            val mediaSource = ProgressiveMediaSource.Factory(dataSourceFactory)
+                .createMediaSource(song.getString(METADATA_KEY_MEDIA_URI).toUri())
+            concatenatingMediaSource.addMediaSource(mediaSource)
+        }
+        return concatenatingMediaSource
+    }
+
+    fun asMediaItems() = songs.map { song ->
+        val desc = MediaDescriptionCompat.Builder()
+            .setMediaUri(song.getString(METADATA_KEY_MEDIA_URI).toUri())
+            .setTitle(song.description.title)
+            .setSubtitle(song.description.subtitle)
+            .setMediaId(song.description.mediaId)
+            .setIconBitmap(song.description.iconBitmap)
+            .build()
+        MediaBrowserCompat.MediaItem(desc, FLAG_PLAYABLE)
+    }
+
+    private val onReadyListeners = mutableListOf<(Boolean) -> Unit>()
+
+    private var state: State = State.STATE_CREATED
+        set(value) {
+            if (value == State.STATE_INITIALIZED || value == State.STATE_ERROR) {
+                synchronized(onReadyListeners) {
+                    field = value
+                    onReadyListeners.forEach { listener ->
+                        listener(state == State.STATE_INITIALIZED)
+                    }
+                }
+            } else {
+                field = value
+            }
+        }
+
+    fun whenReady(action: (Boolean) -> Unit): Boolean {
+        if (state == State.STATE_CREATED || state == State.STATE_INITIALIZING) {
+            onReadyListeners += action
+            return false
+        } else {
+            action(state == State.STATE_INITIALIZED)
+            return true
+        }
+    }
+
     fun getAllSongs(): List<Song> {
         val list = ArrayList<Song>()
         val uri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI
-        val selection = MediaStore.Audio.Media.IS_MUSIC  + " != 0"
+        val selection = MediaStore.Audio.Media.IS_MUSIC + " != 0"
         val sortOrder = MediaStore.Audio.Media.TITLE
         val projection = arrayOf(
             MediaStore.Audio.Media._ID,
@@ -47,4 +125,11 @@ class MusicDataSource(
         cursor?.close()
         return list
     }
+}
+
+enum class State {
+    STATE_CREATED,
+    STATE_INITIALIZING,
+    STATE_INITIALIZED,
+    STATE_ERROR
 }
